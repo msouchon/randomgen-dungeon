53959c67a60c568429d50dd3550e8d99b02ca3e5
Marcus Souchon <marcus_s@live.com.au>
2019-10-16 20:59:17 +1100

    implemented walls

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class MeshGenerator : MonoBehaviour
 {
+	public int wallSize = 10;
+	public MeshFilter walls;
 	public SquareGrid sGrid;
 	List<Vector3> vertices;
 	List<int> triangles;
 
+	// Given a vertex index it matches to the triangles that contain that vertex index
+	Dictionary<int,List<Triangle>> triangleDict = new Dictionary<int,List<Triangle>>();
+	// Stores if we have checked that this vertex index has already been looked at to form a wall
+	HashSet<int> checkedVertIndex = new HashSet<int>();
+	// Stores a list of vertex indexes which correspond to walls
+	List<List<int>> wallVertPaths = new List<List<int>>();
+
 	public void generateMesh(int[,] map, float squareSize) {
+
+		triangleDict.Clear();
+		checkedVertIndex.Clear();
+		wallVertPaths.Clear();
+
 		sGrid = new SquareGrid(map, squareSize);
 
 		int gridX = sGrid.grid.GetLength(0);
@@ -29,6 +43,36 @@ public class MeshGenerator : MonoBehaviour
 		mesh.vertices = vertices.ToArray();
 		mesh.triangles = triangles.ToArray();
 		mesh.RecalculateNormals();
+
+		generateWallMesh();
+	}
+
+	void generateWallMesh() {
+		buildWallVertPaths();
+
+		Mesh wallMesh = new Mesh();
+		List<int> wallTriangles = new List<int>();
+		List<Vector3> wallVertices = new List<Vector3>();
+
+		foreach (List<int> wallPath in wallVertPaths) {
+			for (int i = 0; i < wallPath.Count - 1; i++) {
+				int current = wallVertices.Count;
+				wallVertices.Add(vertices[wallPath[i]]);
+				wallVertices.Add(vertices[wallPath[i + 1]]);
+				wallVertices.Add(vertices[wallPath[i]] - wallSize * Vector3.up);
+				wallVertices.Add(vertices[wallPath[i + 1]] - wallSize * Vector3.up);
+
+				wallTriangles.Add(current + 3);
+				wallTriangles.Add(current);
+				wallTriangles.Add(current + 1);
+				wallTriangles.Add(current);
+				wallTriangles.Add(current + 3);
+				wallTriangles.Add(current + 2);
+			}
+		}
+		wallMesh.vertices = wallVertices.ToArray();
+		wallMesh.triangles = wallTriangles.ToArray();
+		walls.mesh = wallMesh;
 	}
 
 	void buildMeshFromSquare(Square s) {
@@ -89,6 +133,11 @@ public class MeshGenerator : MonoBehaviour
 			// Filled square
 			case 15:
 				sNodes.Add(s.topLeft); sNodes.Add(s.topRight); sNodes.Add(s.botRight); sNodes.Add(s.botLeft);
+				// As this is a filled square it will not be a part of the walls
+				checkedVertIndex.Add(s.topLeft.vertIndex);
+				checkedVertIndex.Add(s.topRight.vertIndex);
+				checkedVertIndex.Add(s.botRight.vertIndex);
+				checkedVertIndex.Add(s.botLeft.vertIndex);
 				break;
 		}
 		// Give each vertex index to the triangles list if it doesnt already have it
@@ -101,18 +150,99 @@ public class MeshGenerator : MonoBehaviour
 		}
 		// Make the triangles
 		if (sNodes.Count >= 3) {
-			triangles.Add(sNodes[0].vertIndex); triangles.Add(sNodes[1].vertIndex); triangles.Add(sNodes[2].vertIndex);
+			addTriangle(sNodes[0], sNodes[1], sNodes[2]);
 		}
 		if (sNodes.Count >= 4) {
-			triangles.Add(sNodes[0].vertIndex); triangles.Add(sNodes[2].vertIndex); triangles.Add(sNodes[3].vertIndex);
+			addTriangle(sNodes[0], sNodes[2], sNodes[3]);
 		}
 		if (sNodes.Count >= 5) {
-			triangles.Add(sNodes[0].vertIndex); triangles.Add(sNodes[3].vertIndex); triangles.Add(sNodes[4].vertIndex);
+			addTriangle(sNodes[0], sNodes[3], sNodes[4]);
 		}
 		if (sNodes.Count >= 6) {
-			triangles.Add(sNodes[0].vertIndex); triangles.Add(sNodes[4].vertIndex); triangles.Add(sNodes[5].vertIndex);
+			addTriangle(sNodes[0], sNodes[4], sNodes[5]);
 		}
 
+	}
+
+	// Adds a triangle to the list of all triangles
+	// and adds the triangle to the triangle dictionary for
+	// later lookup
+	void addTriangle(Node n1, Node n2, Node n3) {
+		triangles.Add(n1.vertIndex);
+		triangles.Add(n2.vertIndex);
+		triangles.Add(n3.vertIndex);
+
+		Triangle t = new Triangle(n1.vertIndex, n2.vertIndex, n3.vertIndex);
+
+		foreach (int vertIndex in t.vertIndexes) {
+			if (triangleDict.ContainsKey(vertIndex)) {
+				triangleDict[vertIndex].Add(t);
+			}
+			else {
+				List<Triangle> l = new List<Triangle>();
+				l.Add(t);
+				triangleDict.Add(vertIndex, l);
+			}
+		}
+	}
+
+	// Given two vertex indexes returns true if they only share one
+	// triangle in common
+	bool shareOneTriangle(int vertIndexA, int vertIndexB) {
+		int numSharedTriangles = 0;
+		List<Triangle> trianglesA = triangleDict[vertIndexA];
+		foreach (Triangle t in trianglesA) {
+			foreach (int vertIndex in t.vertIndexes) {
+				if (vertIndexB == vertIndex) numSharedTriangles++;
+				if (numSharedTriangles > 1) return false;
+			}
+		}
+		return true;
+	}
+
+	// Given a vertex index looks for a another vertex index which is on the
+	// same wall
+	int getConnectWallVertex(int vertIndex) {
+		List<Triangle> triangles = triangleDict[vertIndex];
+
+		foreach (Triangle t in triangles) {
+			foreach (int vertIndexT in t.vertIndexes) {
+				if (vertIndexT != vertIndex) {
+					if (!checkedVertIndex.Contains(vertIndexT)) {
+						if (shareOneTriangle(vertIndexT, vertIndex))
+							return vertIndexT;
+					}
+				}
+			}
+		}
+		return -1;
+	}
+
+	// Given a vertex thats part of a wall keep finding the rest of
+	// the wall and add it to the wall path specified by the wall index
+	void buildWallVertPath(int vertIndex, int wallIndex) {
+		wallVertPaths[wallIndex].Add(vertIndex);
+		checkedVertIndex.Add(vertIndex);
+		int nextWallVertIndex = getConnectWallVertex(vertIndex);
+		if (nextWallVertIndex > -1) buildWallVertPath(nextWallVertIndex, wallIndex);
+	}
+
+	// For each vertex check if it should be a wall then build a wall
+	void buildWallVertPaths() {
+		for (int vertIndex = 0; vertIndex < vertices.Count; vertIndex++) {
+			if (!checkedVertIndex.Contains(vertIndex)) {
+				int connectedWallVertex = getConnectWallVertex(vertIndex);
+				if (connectedWallVertex > -1) {
+					checkedVertIndex.Add(connectedWallVertex);
+					List<int> wall = new List<int>();
+					wall.Add(vertIndex);
+					wallVertPaths.Add(wall);
+					buildWallVertPath(connectedWallVertex, wallVertPaths.Count-1);
+					// Add original vertex to go all the way round to the start again
+					wallVertPaths[wallVertPaths.Count-1].Add(vertIndex);
+				}
+			}
+		}
 	}
 
 	public class SquareGrid {
@@ -144,6 +274,17 @@ public class MeshGenerator : MonoBehaviour
 							cornerNodes[x, z]);
 				}
 			}
+		}
+	}
+
+	public class Triangle {
+		public int[] vertIndexes;
+
+		public Triangle(int vertIndex1, int vertIndex2, int vertIndex3) {
+			vertIndexes = new int[3];
+			vertIndexes[0] = vertIndex1;
+			vertIndexes[1] = vertIndex2;
+			vertIndexes[2] = vertIndex3;
 		}
 	}
 
