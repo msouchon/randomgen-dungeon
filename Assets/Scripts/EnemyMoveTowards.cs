f818975f4f56f808900b205c653cdab214e19539
Luca Kennedy <lucapkennedy@gmail.com>
2019-10-16 23:18:20 +1100

    move towards and wander basics

---
+++ Assets/Scripts/EnemyMoveTowards.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoveTowards : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float speed;
+    public GameObject player;
+
+    public float MAX_DIST;
+    public float MIN_DIST;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distance < MIN_DIST){
+            rb.MovePosition(Vector3.MoveTowards(transform.position, player.transform.position, -speed));
+        }
+        else if (distance < MAX_DIST){
+            rb.MovePosition(Vector3.MoveTowards(transform.position, player.transform.position, speed));
+        }
+        transform.LookAt(player.transform);
+    }
+
+
+}
---
+++ Assets/Scripts/EnemyWander.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWander : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+
+    public Vector3 point1;
+    public Vector3 point2;
+    public Vector3 point3;
+    public Vector3 point4;
+
+    // state varies between 0-7
+    private int state;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+        state = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance;
+        if (state ==0){
+             distance = Vector3.Distance(transform.position, point1);
+            rb.MovePosition(Vector3.MoveTowards(transform.position, point1, speed));
+            if (distance < 0.2){
+                state += 1;
+            }
+        }
+        else if (state ==1){
+             distance = Vector3.Distance(transform.position, point2);
+
+            rb.MovePosition(Vector3.MoveTowards(transform.position, point2, speed));
+            if (distance < 0.2){
+                state += 1;
+            }
+        }
+        else if (state ==2){
+             distance = Vector3.Distance(transform.position, point3);
+
+            rb.MovePosition(Vector3.MoveTowards(transform.position, point3, speed));
+            if (distance < 0.2){
+                state += 1;
+            }
+        }
+        else if (state ==3){
+             distance = Vector3.Distance(transform.position, point4);
+
+            rb.MovePosition(Vector3.MoveTowards(transform.position, point4, speed));
+            if (distance < 0.2){
+                state += 1;
+            }
+        }
+        
+        state %= 4;
+    }
+}
