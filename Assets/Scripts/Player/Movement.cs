d5524352f64c5e8312d3f09727327dd93865cd6a
Marcus Souchon <marcus_s@live.com.au>
2019-10-17 16:41:05 +1100

    added a camera to follow player

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	public Rigidbody rb;
+	public float speed;
+	public float maxScreenMovement;
+	public Camera c;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate() {
+		Vector3 x, z;
+		x = Vector3.right * Input.GetAxis("Horizontal") * speed; 
+		z = Vector3.forward * Input.GetAxis("Vertical") * speed;
+		rb.MovePosition(rb.transform.position + x + z);
+
+		Vector3 position = c.WorldToScreenPoint(transform.position);
+		Vector3 direction = Input.mousePosition - position;
+		float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.down);
+	}
+}
