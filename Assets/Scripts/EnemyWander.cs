0be61157744049a4584f5cdba04917f6aa3cb92c
Luca Kennedy <lucapkennedy@gmail.com>
2019-10-17 16:54:18 +1100

    Bullet basics

--- Assets/Scripts/EnemyWander.cs
+++ Assets/Scripts/EnemyWander.cs
@@ -11,53 +11,86 @@ public class EnemyWander : MonoBehaviour
     public Vector3 point2;
     public Vector3 point3;
     public Vector3 point4;
-
+    private Vector3 targetPoint;
+    private float Timer = 0;
+    
+    public float WALK_TIME;
     // state varies between 0-7
-    private int state;
+    public int state;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>(); 
         state = 0;
+        targetPoint = point1;
+        transform.position = point4;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        float distance;
+    {        
+        Timer += Time.deltaTime;
+        if (Timer > WALK_TIME){
+            state += 1;
+            state %= 8;
+            Timer %= WALK_TIME;
+        } 
+
+
+        switch (state)
+        {
+            case 0:
+                targetPoint = point1;
+                break;
+            case 2:
+                targetPoint = point2;
+                break;
+            case 4:
+                targetPoint = point3;
+                break;
+            case 6:
+                targetPoint = point4;
+                break;
+
+        }
+        if (state % 2 == 1){
+            rb.MovePosition(Vector3.MoveTowards(transform.position, targetPoint, speed));
+        }
+        else{
+            Timer += Time.deltaTime; // doubles speed of timer counter
+            Vector3 targetDir = targetPoint - transform.position;
+            rb.MoveRotation(Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, targetDir, speed/2, 0.0f)));
+        }
+        
+
+        /*
         if (state ==0){
-             distance = Vector3.Distance(transform.position, point1);
             rb.MovePosition(Vector3.MoveTowards(transform.position, point1, speed));
-            if (distance < 0.2){
+            if (transform.position == point1){
                 state += 1;
             }
         }
-        else if (state ==1){
-             distance = Vector3.Distance(transform.position, point2);
+        else if (state == 1){
+            Vector3 targetDir = point2 - transform.position;
+            rb.MoveRotation(Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, targetDir, speed/2, 0.0f)));
+            if (targetDir == transform.forward){
+                
+                transform.position = new Vector3(10f,10f,10f);
+            }
+        }
 
-            rb.MovePosition(Vector3.MoveTowards(transform.position, point2, speed));
-            if (distance < 0.2){
-                state += 1;
-            }
-        }
+
+
         else if (state ==2){
-             distance = Vector3.Distance(transform.position, point3);
-
-            rb.MovePosition(Vector3.MoveTowards(transform.position, point3, speed));
-            if (distance < 0.2){
+            rb.MovePosition(Vector3.MoveTowards(transform.position, point2, speed));
+            if (transform.position == point2){
                 state += 1;
             }
         }
         else if (state ==3){
-             distance = Vector3.Distance(transform.position, point4);
+            rb.MovePosition(Vector3.MoveTowards(transform.position, point3, speed));
+        } */
 
-            rb.MovePosition(Vector3.MoveTowards(transform.position, point4, speed));
-            if (distance < 0.2){
-                state += 1;
-            }
-        }
-        
-        state %= 4;
     }
 }
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bullet;
+
+    private float timer = 0;
+
+    public GameObject parentObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.ShootBullet();
+    }
+    private void ShootBullet(){
+        GameObject a = Instantiate(bullet) as GameObject;
+        // a.transform.parent = transform;
+        a.transform.position = transform.position;
+        a.transform.forward = transform.forward;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 3){
+            ShootBullet();
+            timer %= 3;
+        }
+    }
+}
