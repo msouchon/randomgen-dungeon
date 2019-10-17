9c77264ec5ab8a7e3565f0fd0dd5a52804d64bca
Marcus Souchon <marcus_s@live.com.au>
2019-10-17 20:28:26 +1100

    made working dash

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -8,15 +8,18 @@ public class Movement : MonoBehaviour
 	public float speed;
 	public float maxScreenMovement;
 	public Camera c;
+	public bool enableMovement = true;
 
 	void Start() {
 		rb = GetComponent<Rigidbody>();
 	}
 	void FixedUpdate() {
-		Vector3 x, z;
-		x = Vector3.right * Input.GetAxis("Horizontal") * speed; 
-		z = Vector3.forward * Input.GetAxis("Vertical") * speed;
-		rb.MovePosition(rb.transform.position + x + z);
+		if (enableMovement) {
+			Vector3 x, z;
+			x = Vector3.right * Input.GetAxis("Horizontal") * speed; 
+			z = Vector3.forward * Input.GetAxis("Vertical") * speed;
+			rb.MovePosition(rb.transform.position + x + z);
+		}
 
 		Vector3 position = c.WorldToScreenPoint(transform.position);
 		Vector3 direction = Input.mousePosition - position;
