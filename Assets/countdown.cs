61bab546fe397fa5c50b9776aa3d41c2bd4205af
k-start <startkie@gmail.com>
2019-10-18 13:07:29 +1100

    cooldown variable

--- Assets/countdown.cs
+++ Assets/countdown.cs
@@ -5,13 +5,15 @@ using UnityEngine.UI;
 
 public class countdown : MonoBehaviour
 {
+    public int cooldown = 3;
     private int TimeLeft = 3;
     private float time = 0.0f;
     private float interpolationPeriod = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        TimeLeft = cooldown;
+        GetComponent<Text>().text = TimeLeft.ToString();
     }
 
     // Update is called once per frame
@@ -23,7 +25,7 @@ public class countdown : MonoBehaviour
             time = time - interpolationPeriod;
             TimeLeft -= 1;
             if(TimeLeft == 0) {
-                TimeLeft = 3;
+                TimeLeft = cooldown;
                 this.transform.parent.gameObject.SetActive(false);
             }
             GetComponent<Text>().text = TimeLeft.ToString();
