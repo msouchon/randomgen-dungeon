b819c941fc5a017c86a8415174a013ad9a366923
Marcus Souchon <marcus_s@live.com.au>
2019-10-23 20:05:17 +1100

    implemented basic lightning between two points using line renderer

---
+++ Assets/Scripts/Lightning.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning : MonoBehaviour
+{
+	public GameObject target;
+
+	public float distThreshold = 3.0f;
+	public float accuracy = 0.5f;
+	public float arcLength = 1f;
+	public float arcVariation = 1f;
+
+	private LineRenderer lr;
+
+	void Start() {
+		lr = GetComponent<LineRenderer>();
+		lr.SetVertexCount(1);
+		StartCoroutine("Bolts");
+	}
+
+	IEnumerator Bolts() {
+		while (true) {
+		Vector3 currPos = transform.position;
+		lr.SetPosition(0, currPos);
+		int currVert = 1;
+		while (Vector3.Distance(currPos, target.transform.position) > distThreshold) {
+			lr.SetVertexCount(currVert + 1);
+			Vector3 dir = target.transform.position - currPos;
+			dir = dir.normalized;
+			dir += new Vector3(Random.value - 0.5f, Random.value - 0.5f, Random.value - 0.5f);
+			dir = dir.normalized;
+			dir *= Random.Range(arcVariation * arcLength, arcLength);
+			dir += currPos;
+			lr.SetPosition(currVert, dir);
+			currVert++;
+			currPos = dir;
+		}
+		lr.SetVertexCount(currVert + 1);
+		lr.SetPosition(currVert, target.transform.position);
+		yield return new WaitForSeconds(.1f);
+		}
+	}
+}
