74c9c4474d1721083c64b732dfef0d67993f0bc4
Luca Kennedy <lucapkennedy@gmail.com>
2019-10-24 18:59:25 +1100

    Basic Menu funtionality, no aesthetics

---
+++ Assets/Scripts/Scene Controllers/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Campaign()
+    {
+        SceneManager.LoadScene("MainScene"); 
+    }
+
+    // Update is called once per frame
+    public void Survival()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+
+    public void instructions(){
+        SceneManager.LoadScene("InstructionScene");
+        
+    }
+}
