52faa389c4c1461cc36f5eb8d478c5db78c409c5
k-start <startkie@gmail.com>
2019-10-26 23:36:02 +1100

    Bullet damage

--- Assets/Scripts/Enemy/EnemyBulletController.cs
+++ Assets/Scripts/Enemy/EnemyBulletController.cs
@@ -4,31 +4,38 @@ using UnityEngine;
 
 public class EnemyBulletController : MonoBehaviour
 {
-	public Vector3 direction;
-	public float speed;
-	public float damage = 25;
+    public Vector3 direction;
+    public float speed;
+    public float damage = 25;
 
-	private Rigidbody rb;
+    private Rigidbody rb;
 
-	void Start() {
-		rb = GetComponent<Rigidbody>();
-	}
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
-	void Update() {
-		rb.MovePosition(rb.transform.position + direction * Time.deltaTime * speed);
-	}
+    void Update()
+    {
+        rb.MovePosition(rb.transform.position + direction * Time.deltaTime * speed);
+    }
 
-	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.tag == "Wall") {
-			RaycastHit hit;
-			Physics.Raycast(transform.position, direction, out hit);
-			direction = Vector3.Reflect(direction, hit.normal);
-		}
-		if (other.gameObject.tag == "Player") {
-			if (other.gameObject.GetComponent<Health>() != null) {
-				other.gameObject.GetComponent<Health>().ApplyDamage(damage);
-			}
-		}
-	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            RaycastHit hit;
+            Physics.Raycast(transform.position, direction, out hit);
+            direction = Vector3.Reflect(direction, hit.normal);
+        }
+        if (other.gameObject.tag == "Player")
+        {
+            if (other.gameObject.GetComponent<PlayerHealth>() != null)
+            {
+                other.gameObject.GetComponent<PlayerHealth>().ApplyDamage(damage);
+                Destroy(this.gameObject);
+            }
+        }
+    }
 
 }
