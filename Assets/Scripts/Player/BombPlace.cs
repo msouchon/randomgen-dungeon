a3013017ba119a157305f194dbf6bedd83c89d09
Marcus Souchon <marcus_s@live.com.au>
2019-10-26 18:41:44 +1100

    implemented bomb weapon on space

---
+++ Assets/Scripts/Player/BombPlace.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombPlace : MonoBehaviour
+{
+	public GameObject bomb;
+
+	void Update() {
+		if (Input.GetKeyDown("space")) {
+			Instantiate(bomb, transform.position + transform.forward, transform.rotation);
+		}
+	}
+}
