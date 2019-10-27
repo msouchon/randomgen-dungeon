692a3d2318a33dad46215826fbf38a6420cbbe9a
k-start <startkie@gmail.com>
2019-10-27 15:07:23 +1100

    reset player skills on change

--- Assets/Scripts/Scene Controllers/MainMenu.cs
+++ Assets/Scripts/Scene Controllers/MainMenu.cs
@@ -8,17 +8,20 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     public void Campaign()
     {
+        Levels.playerSpells = new List<Spells.SpellsEnum>();
         SceneManager.LoadScene("MainScene");
     }
 
     // Update is called once per frame
     public void Survival()
     {
+        Levels.playerSpells = new List<Spells.SpellsEnum>();
         SceneManager.LoadScene("SurvivalScene");
     }
 
     public void instructions()
     {
+        Levels.playerSpells = new List<Spells.SpellsEnum>();
         SceneManager.LoadScene("TutorialScene");
 
     }
